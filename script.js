85aaf0ece29953a974bf6037f8bb31f01135ae0b
chriskoder <gerokristof0909@gmail.com>
2019-12-26 22:30:26 +0100

    Deleted useless files

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 const available = ["rock", "paper", "scissors"];
-let run = true;
+const score = document.querySelector(".score");
+const resultText = document.querySelector('.result');
+const note = document.querySelector(".note");
+let pGuess, cGuess, pScore, cScore;
+let init = (function (){
+    resultText.textContent = ' ';
+    pScore = 0;
+    cScore = 0;
+    updateScore();
+    document.querySelector('.rock').addEventListener("click", function(){
+        simulateRound('rock');
+    });
+    document.querySelector('.paper').addEventListener("click", function(){
+        simulateRound('paper');
+    });
+    document.querySelector('.scissors').addEventListener("click", function(){
+        simulateRound('scissors');
+    });
+})();
+function simulateRound(str) {
+    pGuess = str;
+    cGuess = computerPlay();
+    note.textContent = playRound();
+}
+function updateScore() {
+    score.textContent = pScore + " - " + cScore;
+}
 function computerPlay() {
     let r = Math.floor(Math.random() * 3);
-    let cGuess = available[r];
+    cGuess = available[r];
     return cGuess;
 }
 function playRound() {
-    let pGuess = prompt('Rock, paper, or scissors? Write "stop" for exit.');
-    pGuess = pGuess.toLowerCase();
-    let cGuess = computerPlay();
     let result;
     if (pGuess === cGuess) {
-        result = "It's a draw! Your " + pGuess + " doesn't beat the computer's " + pGuess + ".";
+        result = "Your " + pGuess + " doesn't beat the computer's " + pGuess + ".";
+        resultText.textContent = "It's a draw!"
     } else if (pGuess==='rock' && cGuess==='scissors') {
-        result = "You win! Rock beats scissors.";
+        result = "Rock beats scissors.";
+        resultText.textContent = "You win"
+        pScore++;
     } else if (pGuess==='rock' && cGuess==='paper') {
-        result = "You lose! Paper beats rock.";
+        result = "Paper beats rock.";
+        resultText.textContent = "You lose!"
+        cScore++;
     } else if (pGuess==='scissors' && cGuess==='paper') {
-        result = "You win! Scissors beat paper.";
+        result = "Scissors beat paper.";
+        resultText.textContent = "You win!"
+        pScore++;
     } else if (pGuess==='scissors' && cGuess==='rock') {
-        result = "You lose! Rock beats scissors.";
+        result = "Rock beats scissors.";
+        resultText.textContent = "You lose!"
+        cScore++;
     } else if (pGuess==='paper' && cGuess==='scissors') {
-        result = "You lose! Scissors beat paper.";
+        result = "Scissors beat paper.";
+        resultText.textContent = "You lose!"
+        cScore++;
     } else if (pGuess==='paper' && cGuess==='rock') {
-        result = "You win! Rock beats scissors.";
-    } else if (pGuess==='stop') {
-        run = false;
+        result = "Paper beats rock.";
+        resultText.textContent = "You win!"
+        pScore++;
     }
+    updateScore();
     return result;
-}
-while(run) {
-    console.log(playRound())
 }
